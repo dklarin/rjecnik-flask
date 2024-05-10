50e31f528dd1b62ef7383e376bec453b84f6c6d4
dklarin <danijel.klarin@gmail.com>
2024-05-10 22:39:47 +0200

    wks prebačen

--- komponente/rijeci_sve.py
+++ komponente/rijeci_sve.py
@@ -22,6 +22,8 @@ kljucevi_json = ['link', 'skupina']
 linkovi = [{kljuc: vrijednost for kljuc, vrijednost in zip(kljucevi_json, vrijednosti)} 
                 for vrijednosti in zip(link, skupina)] 
 
+retci = wks.get_all_records()
+
 @rijeci.route('/rijeci/<jezik_skupina_sort>/')
 def rjecnik(jezik_skupina_sort):  
 
@@ -31,16 +33,16 @@ def rjecnik(jezik_skupina_sort):
     skupina = splitani_string[1]
     sort = splitani_string[2]
    
-    retci = wks.get_all_records()
+    
     
     if skupina == 'moja_druzina':
-        retci = [item for item in retci if item['skupina'] == 'moja družina']
+        rijeci = [item for item in retci if item['skupina'] == 'moja družina']
     elif skupina == 'solski_pribor':
-        retci = [item for item in retci if item['skupina'] == 'šolski pribor']   
+        rijeci = [item for item in retci if item['skupina'] == 'šolski pribor']   
     elif skupina == 'kaj_je_to_kdo_je_to':
-        retci = [item for item in retci if item['skupina'] == 'kaj je to kdo je to']
+        rijeci = [item for item in retci if item['skupina'] == 'kaj je to kdo je to']
     elif skupina == 'sve':
-        retci = retci         
+        rijeci = retci         
     else:
         print('Ne postoji takva riječ!')
         return render_template('404.html'), 404
@@ -67,9 +69,9 @@ def rjecnik(jezik_skupina_sort):
 
     if sort == 'sortiraj':
         # sortiranje niza riječi koristeći prilagođenu funkciju za sortiranje
-        sortirane_rijeci = sorted(retci, key=lambda x: custom_sort_key(x, jezik)) 
+        sortirane_rijeci = sorted(rijeci, key=lambda x: custom_sort_key(x, jezik)) 
     else:
-        sortirane_rijeci = retci
+        sortirane_rijeci = rijeci
     
     #print(skupina)
 
